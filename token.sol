923616df59e3d4ea26c9e73a9ac5150430411e7f
shivashivu0 <148582234+shivashivu0@users.noreply.github.com>
2023-10-25 23:29:35 +0530

    Update token.sol

--- token.sol
+++ token.sol
@@ -1,1 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
 
+contract Token {
+
+  string public name;
+  string public symbol;
+  uint256 public totalSupply;
+
+  mapping(address => uint256) public balances;
+
+  event TokensMinted(address indexed receiver, uint256 quantity);
+  event TokensBurned(address indexed holder, uint256 quantity);
+
+  constructor() {
+      name = "Dollar";
+      symbol = "$";
+      totalSupply = 300;
+      balances[msg.sender] = totalSupply;
+  }
+
+  function mint(address receiver, uint256 quantity) public {
+    require(receiver != address(0), "Invalid receiver address");
+
+    totalSupply += quantity;
+    balances[receiver] += quantity;
+
+    emit TokensMinted(receiver, quantity);
+  }
+
+  function burn(uint256 quantity) public {
+    require(balances[msg.sender] >= quantity, "Insufficient balance");
+
+    totalSupply -= quantity;
+    balances[msg.sender] -= quantity;
+
+    emit TokensBurned(msg.sender, quantity);
+  }
+}
